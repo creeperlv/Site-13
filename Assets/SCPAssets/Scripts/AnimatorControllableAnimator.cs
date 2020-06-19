c6ec184eb1364710f1eb60924a5722a28c9160e5
Creeper Lv <creeperlv38@outlook.com>
2020-06-20 00:27:17 +0800

    0.1.89.0 Added a controller. Added a few packages.

---
+++ Assets/SCPAssets/Scripts/AnimatorControllableAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Site13Kernel
+{
+
+    public class AnimatorControllableAnimator : MonoBehaviour
+    {
+        public int TargetTrigger;
+        public List<string> TriggerNames;
+        int LastTrigger = 0;
+        public Animator ControlledAnimator;
+        void Start()
+        {
+
+        }
+        void Update()
+        {
+            if (LastTrigger != TargetTrigger)
+            {
+                ControlledAnimator.SetTrigger(TriggerNames[TargetTrigger]);
+                LastTrigger = TargetTrigger;
+            }
+        }
+    }
+
+}
