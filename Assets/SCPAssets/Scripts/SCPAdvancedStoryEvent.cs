2b282a82ecaf07871651df07047f62045967f7a3
Creeper Lv <creeperlv38@outlook.com>
2020-07-03 22:44:01 +0800

    0.1.89.0 -Adjustments on ServerV2. -Added a new trigger-event way.

---
+++ Assets/SCPAssets/Scripts/SCPAdvancedStoryEvent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Site13Kernel
+{
+    public class SCPAdvancedStoryEvent : MonoBehaviour
+    {
+        public UnityEvent EnterEvent;
+        public UnityEvent ExitEvent;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.GetComponent<SCPFirstController>() != null)
+            {
+                EnterEvent.Invoke();
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.GetComponent<SCPFirstController>() != null)
+            {
+                ExitEvent.Invoke();
+            }
+        }
+    }
+}
