588f6be1f4a245f7b36d4ae9beaf2e1937a55b0e
Creeper Lv <creeperlv38@outlook.com>
2020-08-08 15:36:21 +0800

    0.1.90.0 -Rename: Watch Mode -> Observe Mode -Improved Observe Mode: Added an overlay to indicate status. -Improved Observe Mode: Mouse sensitivity will be 1/20 to improved observe experience. -Fixed some logical bugs between observe mode and BagSystem V2, V3.

--- Assets/SCPAssets/Scripts/SCPBagSystemV2.cs
+++ Assets/SCPAssets/Scripts/SCPBagSystemV2.cs
@@ -19,6 +19,7 @@ namespace Site13Kernel
         public string DefKeyCard = "L-3-K";
         public string DefPhone = "LUMIA950";
         public float WatchModeFov = 25;
+        public CanvasGroup ObserveModeOverlay;
         [Serializable]
         public class BagItem
         {
@@ -27,6 +28,7 @@ namespace Site13Kernel
             public GameObject Item;
         }
         public List<BagItem> bagItems = new List<BagItem>();
+        bool WatchMode00 = false;
         private void Start()
         {
             if (!GameInfo.CurrentGame.FlagsGroup.ContainsKey($"BAG.{BAGOwner}.1"))
@@ -132,14 +134,16 @@ namespace Site13Kernel
         {
             if (GameInfo.CurrentGame.isPaused == false)
             {
-                if (isCurrentItemV3Enabled == true)
-                {
-                    if (CurrentItem.IsOnOperation() == false) CheckSwitchItem();
-                }
-                else
-                {
-                    CheckSwitchItem();
-                }
+                if (
+                   GameInfo.CurrentGame.isWatching == false)
+                    if (isCurrentItemV3Enabled == true)
+                    {
+                        if (CurrentItem.IsOnOperation() == false) CheckSwitchItem();
+                    }
+                    else
+                    {
+                        CheckSwitchItem();
+                    }
             }
 
             if (Input.GetButton("Watch"))
@@ -149,132 +153,148 @@ namespace Site13Kernel
                 {
                     if (Camera.main.fieldOfView > WatchModeFov)
                     {
-                        Camera.main.fieldOfView -= Time.deltaTime * (100f / 40f) * (60f - WatchModeFov);
+                        if (WatchMode00 == false)
+                            WatchMode00 = true;
+                        if (ObserveModeOverlay != null)
+                            ObserveModeOverlay.alpha += Time.deltaTime * 2;//* (GameInfo.CurrentGame.UsingFOV - WatchModeFov);
+                        Camera.main.fieldOfView -= Time.deltaTime * 2 * (GameInfo.CurrentGame.UsingFOV - WatchModeFov);
                     }
                     else
                     {
+                        if (ObserveModeOverlay != null)
+                            ObserveModeOverlay.alpha = 1;
                         GameInfo.CurrentGame.isAiming = true;
                         Camera.main.fieldOfView = WatchModeFov;
                         foreach (var item in bagItems)
                         {
                             item.Item.SetActive(false);
                         }
+                        GameInfo.CurrentMouseSen = GameInfo.TargetMouseSen / 20;
                         GameInfo.CurrentGame.isWatching = true;
                     }
                 }
             }
             else
             {
-                if (
-                   GameInfo.CurrentGame.isWatching == true)
+                if (GameInfo.CurrentGame.isWatching == true | WatchMode00 == true)
                 {
                     if (Camera.main.fieldOfView < GameInfo.CurrentGame.UsingFOV)
                     {
-                        Camera.main.fieldOfView += Time.deltaTime * 100;
+                        if (ObserveModeOverlay != null)
+                            ObserveModeOverlay.alpha -= Time.deltaTime * 2;//* (GameInfo.CurrentGame.UsingFOV - WatchModeFov) / 5;
+                        Camera.main.fieldOfView += Time.deltaTime * 2 * (GameInfo.CurrentGame.UsingFOV - WatchModeFov);
                     }
                     else
                     {
-
+                        if (ObserveModeOverlay != null)
+                            ObserveModeOverlay.alpha = 0;
                         GameInfo.CurrentGame.isWatching = false;
+                        WatchMode00 = false;
+                        GameInfo.CurrentMouseSen = GameInfo.TargetMouseSen;
                         GameInfo.CurrentGame.isAiming = false; SetItem();
                         Camera.main.fieldOfView = GameInfo.CurrentGame.UsingFOV;
                     }
                 }
             }
-            try
-            {
-                string id2 = id1;
-                switch (currentItem)
+            if (
+               GameInfo.CurrentGame.isWatching == false)
+                try
                 {
-                    case 0:
-                        {
-
-                        }
-                        break;
-                    case 1:
-                        {
-                            //Primary Weapon
-                            try
-                            {
-                                id2 = GameInfo.CurrentGame.FlagsGroup[$"BAG.{BAGOwner}.1"];
-                            }
-                            catch (Exception)
-                            {
-                            }
-                        }
-                        break;
-                    case 2:
-                        {
-                            try
-                            {
-                                id2 = GameInfo.CurrentGame.FlagsGroup[$"BAG.{BAGOwner}.2"];
-                            }
-                            catch (Exception)
-                            {
-                            }
-                        }
-                        break;
-                    default:
-                        break;
-                }
-                if (id1 != id2)
-                {
-                    foreach (var item in bagItems)
+                    string id2 = id1;
+                    switch (currentItem)
                     {
-                        if (item.ID == id1)
-                        {
-                            item.Item.SetActive(false);
+                        case 0:
+                            {
+
+                            }
                             break;
+                        case 1:
+                            {
+                                //Primary Weapon
+                                try
+                                {
+                                    id2 = GameInfo.CurrentGame.FlagsGroup[$"BAG.{BAGOwner}.1"];
+                                }
+                                catch (Exception)
+                                {
+                                }
+                            }
+                            break;
+                        case 2:
+                            {
+                                try
+                                {
+                                    id2 = GameInfo.CurrentGame.FlagsGroup[$"BAG.{BAGOwner}.2"];
+                                }
+                                catch (Exception)
+                                {
+                                }
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                    if (id1 != id2)
+                    {
+                        foreach (var item in bagItems)
+                        {
+                            if (item.ID == id1)
+                            {
+                                item.Item.SetActive(false);
+                                break;
+                            }
+                        }
+                        id1 = id2;
+                        foreach (var item in bagItems)
+                        {
+                            if (item.ID == id1)
+                            {
+                                item.Item.SetActive(true);
+                                ApplyItem(item.Item);
+                                break;
+                            }
                         }
                     }
-                    id1 = id2;
-                    foreach (var item in bagItems)
+                }
+                catch (Exception)
+                {
+                }
+
+            if (
+               GameInfo.CurrentGame.isWatching == false)
+                if (isCurrentItemV3Enabled == true)
+                {
+                    //Deal with keys...
+
+                    if (Input.GetButton("Fire1"))
                     {
-                        if (item.ID == id1)
-                        {
-                            item.Item.SetActive(true);
-                            ApplyItem(item.Item);
-                            break;
-                        }
+                        if (CurrentItem.IsOnOperation() == false)
+                            CurrentItem.Primary();
+                    }
+                    else if (Input.GetButtonUp("Fire1"))
+                    {
+                        CurrentItem.UnPrimary();
+                    }
+                    //if (Input.GetButton("Fire2"))
+                    //{
+                    //    if (CurrentItem.IsOnOperation() == false)
+                    //        CurrentItem.Secondary();
+                    //}
+                    //else if (Input.GetButtonUp("Fire2"))
+                    //{
+                    //    CurrentItem.UnSecondary();
+                    //}
+                    if (Input.GetButtonDown("Fight"))
+                    {
+                        if (CurrentItem.IsOnOperation() == false)
+                            CurrentItem.Fight();
+                    }
+                    if (Input.GetButtonDown("FlashLight"))
+                    {
+                        if (CurrentItem.IsOnOperation() == false)
+                            CurrentItem.FlashLight();
                     }
                 }
-            }
-            catch (Exception)
-            {
-            }
-            if (isCurrentItemV3Enabled == true)
-            {
-                //Deal with keys...
-
-                if (Input.GetButton("Fire1"))
-                {
-                    if (CurrentItem.IsOnOperation() == false)
-                        CurrentItem.Primary();
-                }
-                else if (Input.GetButtonUp("Fire1"))
-                {
-                    CurrentItem.UnPrimary();
-                }
-                //if (Input.GetButton("Fire2"))
-                //{
-                //    if (CurrentItem.IsOnOperation() == false)
-                //        CurrentItem.Secondary();
-                //}
-                //else if (Input.GetButtonUp("Fire2"))
-                //{
-                //    CurrentItem.UnSecondary();
-                //}
-                if (Input.GetButtonDown("Fight"))
-                {
-                    if (CurrentItem.IsOnOperation() == false)
-                        CurrentItem.Fight();
-                }
-                if (Input.GetButtonDown("FlashLight"))
-                {
-                    if (CurrentItem.IsOnOperation() == false)
-                        CurrentItem.FlashLight();
-                }
-            }
         }
         string id1 = "";
         IHandItem CurrentItem = null;
