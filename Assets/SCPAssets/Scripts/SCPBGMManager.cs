dddf31323f9c3cc50bf4c54537b321a0478717a7
Creeper Lv <creeperlv38@outlook.com>
2020-09-06 14:25:10 +0800

    0.1.90.0 -Changed old grey background into a video. -BGMManager should be able perform transition when changing BGM.

--- Assets/SCPAssets/Scripts/SCPBGMManager.cs
+++ Assets/SCPAssets/Scripts/SCPBGMManager.cs
@@ -10,12 +10,71 @@ namespace Site13Kernel
         public List<string> Alias;
         public List<AudioClip> RealBGMs;
         public AudioSource audioSource;
+        public AudioSource audioSource1;
+        bool State0 = false;//False - audio0,True - Audio1
+        bool isWorking = false;
         public void ChangeBGM(string TargetAlia)
         {
             CurrentAlia = TargetAlia;
             var インデックス = Alias.IndexOf(TargetAlia);
-            audioSource.clip = RealBGMs.ElementAt(インデックス);
-            audioSource.Play();
+            if (State0 == false)
+            {
+                audioSource1.clip = RealBGMs.ElementAt(インデックス);
+                audioSource1.volume = 0;
+                audioSource1.Play(); 
+                State0 = true;
+            }
+            else
+            if (State0 == true)
+            {
+                audioSource.clip = RealBGMs.ElementAt(インデックス);
+                audioSource.volume = 0;
+                audioSource.Play(); 
+                State0 = false;
+            }
+            if (isWorking == false)
+            {
+                StartCoroutine(BGMTransition());
+            }
+            else
+            {
+                isWorking = false;
+            }
+        }
+        public IEnumerator BGMTransition()
+        {
+            isWorking = true;
+            float TimeDelta = 0;
+            while (TimeDelta < 1)
+            {
+
+                if (State0 == false)
+                {
+
+                    if(audioSource.volume<.6f)
+                    audioSource.volume += Time.deltaTime*0.6f;
+                    if(audioSource1.volume>0f)
+                    audioSource1.volume -= Time.deltaTime*0.6f;
+                }
+                else
+                {
+
+                    if(audioSource1.volume<.6f)
+                    audioSource1.volume += Time.deltaTime * 0.6f;
+                    if(audioSource.volume>0f)
+                    audioSource.volume -= Time.deltaTime * 0.6f;
+                }
+
+
+                if (isWorking == false)
+                {
+                    isWorking = true;
+                    TimeDelta = 0;
+                }
+                yield return null;
+            }
+            yield return null;
+
         }
         public string CurrentAlia;
         private void Start()
@@ -25,11 +84,15 @@ namespace Site13Kernel
         }
         public void StopBGM()
         {
-            audioSource.Stop();
+            if (State0 == false)
+                audioSource.Stop();
+            else audioSource1.Stop();
         }
         public void PlayBGM()
         {
-            audioSource.Play();
+            if (State0 == false)
+                audioSource.Play();
+            else audioSource1.Play();
         }
     }
 
