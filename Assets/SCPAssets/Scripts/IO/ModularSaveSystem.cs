f8ed125fbd7f269b1ab433a8fad8cc614d797555
Creeper Lv <creeperlv38@outlook.com>
2020-10-02 00:18:45 +0800

    0.1.90.0 -Updated Unity3D to 2019.4.11f1. -Continue working on new save system.

--- Assets/SCPAssets/Scripts/IO/ModularSaveSystem.cs
+++ Assets/SCPAssets/Scripts/IO/ModularSaveSystem.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 namespace Site13Kernel.IO
 {
 
     public class ModularSaveSystem : MonoBehaviour, ISave
     {
-        public int SaveSystemID;
-        public string SavePath { get { return "Undefined."; } }
+        public string SaveSystemID;
+        /// <summary>
+        /// Parent path.
+        /// </summary>
+        public string SavePath { get; private set; }
         public Dictionary<string, ModularSaveSystemModule> Modules = new Dictionary<string, ModularSaveSystemModule>();
         public void Load()
         {
@@ -24,18 +28,20 @@ namespace Site13Kernel.IO
                 item.Value.Save();
             }
         }
-
-        // Start is called before the first frame update
         void Start()
         {
+            if(Application.platform == RuntimePlatform.WindowsPlayer)
+            {
+                SavePath = ".";
+            }
+            else if (Application.platform == RuntimePlatform.WSAPlayerX64 || Application.platform == RuntimePlatform.WSAPlayerX64 || Application.platform == RuntimePlatform.LinuxPlayer || Application.platform == RuntimePlatform.OSXPlayer)
+            {
+                SavePath = Application.dataPath;
+            }
+            SavePath = Path.Combine(SavePath, SaveSystemID);
             GameInfo.CurrentGame.CurrentSceneSaveSystem = this;
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
     }
 
 }
