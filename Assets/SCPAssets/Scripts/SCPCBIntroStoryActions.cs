a90f9c5ad301e1c93cfa8782720c138ca74e6485
Creeper Lv <creeperlv38@outlook.com>
2020-10-16 17:20:28 +0800

    0.1.90.0 -Continue work on new Intro animation.

--- Assets/SCPAssets/Scripts/SCPCBIntroStoryActions.cs
+++ Assets/SCPAssets/Scripts/SCPCBIntroStoryActions.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Site13Kernel.IO;
+using UnityEngine.UI;
+
 namespace Site13Kernel.Stories
 {
     public class SCPCBIntroStoryActions : BytableBehavior
@@ -30,14 +32,75 @@ namespace Site13Kernel.Stories
         public bool Action7_CloseDoor2 = false;
         private bool Action7_CloseDoor2_ = false;
 
+        public bool Action8_OpenDoor3 = false;
+        private bool Action8_OpenDoor3_ = false;
+
+        public bool Action9_CloseDoor3 = false;
+        private bool Action9_CloseDoor3_ = false;
+
+        public bool Action10_OpenDoor3 = false;
+        private bool Aciton10_OpenDoor3_ = false;
+
+        public bool Action11_079InCharge = false;
+        private bool Action11_079InCharge_ = false;
+
+        public bool Action12_OutShelter = false;
+        private bool Action12_OutShelter_ = false;
+
+        public bool Action13_Explosion = false;
+        private bool Action13_Explosion_ = false;
+
+        public bool Constant_Action0_Blink = false;
+        private bool Blink_0_0 = false;
+        private float Blink_0_1 = 0;
+        private float Blink_0_2 = 1;
 
         public SCPDoor Door0;
         public SCPDoor Door1;
         public SCPDoor Door2;
         public SCPDoor Door3;
         public AudioSource CBAudio00;
+        public AudioClip _079InCharge;
+        public AudioClip OutShelter;
+        public Image BlackCover;
+        public AudioSource Explosion;
         void Update()
         {
+            if (Constant_Action0_Blink == true)
+            {
+                if (Blink_0_0 == false)
+                {
+                    var c = BlackCover.color;
+                    c.a -= Time.deltaTime*1.5f;
+                    Blink_0_2 -= Time.deltaTime * 1.5f;
+                    BlackCover.color = c;
+                    if (Blink_0_2 <= 0)
+                    {
+                        Blink_0_2 = 0;
+                        Blink_0_0 = true;
+                    }
+                }
+                else
+                {
+                    if (Blink_0_1 < 1)
+                    {
+                        Blink_0_1 += Time.deltaTime*1.5f;
+                    }
+                    else
+                    {
+                        var c = BlackCover.color;
+                        c.a += Time.deltaTime * 1.5f;
+                        Blink_0_2 += Time.deltaTime * 1.5f;
+                        BlackCover.color = c;
+                        if (Blink_0_2 >= 1)
+                        {
+                            Blink_0_0 = false;
+                            Blink_0_1 = 0;
+                            Blink_0_2 = 1;
+                        }
+                    }
+                }
+            }
             if (Action0_ShowSubtitle00 == true)
             {
                 if (Action0_ShowSubtitle00_ == false)
@@ -100,6 +163,15 @@ namespace Site13Kernel.Stories
                 {
                     Action7_CloseDoor2_ = true;
                     StartCoroutine(Door2.Close());
+                }
+            }
+            if (Action11_079InCharge == true)
+            {
+                if (Action11_079InCharge_ == false)
+                {
+                    Action11_079InCharge_ = true;
+                    CBAudio00.clip = _079InCharge;
+                    CBAudio00.Play();
                 }
             }
         }
