22f931f0990bb61940408776c4ff104af8cc8cb1
Creeper Lv <creeperlv38@outlook.com>
2020-10-18 22:56:45 +0800

    0.1.90.0 -Intro scene should be loaded in background now.

--- Assets/SCPAssets/Scripts/JumpScene.cs
+++ Assets/SCPAssets/Scripts/JumpScene.cs
@@ -8,9 +8,43 @@ namespace Site13Kernel
     public class JumpScene : SCPStoryNodeBaseCode
     {
         public int SceneID = 0;
+        public bool PreLoad = false;
+        AsyncOperation SceneLoad;
+        public override void StartScript()
+        {
+            if (PreLoad == true)
+            {
+
+                Debug.Log("Preload Scene");
+                StartCoroutine(LoadAsync());
+            }
+        }
+        IEnumerator LoadAsync()
+        {
+            yield return new WaitForSeconds(1f);
+            SceneLoad = SceneManager.LoadSceneAsync(SceneID);
+            SceneLoad.allowSceneActivation = false;
+            SceneLoad.completed += SceneLoad_completed;
+            yield return null;
+        }
+
+        private void SceneLoad_completed(AsyncOperation obj)
+        {
+            obj.allowSceneActivation = false;
+        }
+
         public override void StartStory()
         {
-            SceneManager.LoadScene(SceneID);
+            if (PreLoad == false)
+            {
+                SceneManager.LoadScene(SceneID);
+            }
+            else
+            {
+                SceneLoad.allowSceneActivation = true;
+            }
+            //SceneLoad.allowSceneActivation = true;
+            //SceneManager.LoadScene(SceneID);
         }
     }
 
