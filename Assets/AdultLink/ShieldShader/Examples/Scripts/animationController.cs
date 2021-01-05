78d34563cf2d829bc6de3d0dd3b3e49e51806510
Creeper Lv <creeperlv38@outlook.com>
2021-01-05 20:41:19 +0800

    0.1.90.0 -Organizing root folder of Assets. -Working on remaking of the new elevator platform. -Added a static-reference of ShieldShader by @AdultLink.

---
+++ Assets/AdultLink/ShieldShader/Examples/Scripts/animationController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            anim.SetTrigger("activateTransition");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            anim.SetTrigger("activateTransition_inv");
+        }
+    }
+}
