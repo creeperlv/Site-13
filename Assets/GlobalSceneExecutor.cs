05fcb075d7f9ea656dbe4c9351672e1d7313e613
Creeper Lv <creeperlv38@outlook.com>
2021-02-07 19:56:29 +0800

    0.1.90.0 -Working on the Modular Scene.

---
+++ Assets/GlobalSceneExecutor.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Site13Kernel.DynamicScene
+{
+
+    public class GlobalSceneExecutor : MonoBehaviour
+    {
+        public static GlobalSceneExecutor CurrentExecutor;
+        public GameObject Player;
+        public LightingSettings lightingSettings;
+        void Start()
+        {
+            CurrentExecutor = this;
+        }
+        public void Invoke(Action a)
+        {
+            a();
+        }
+        public void SartCoroutine(IEnumerator enumerator)
+        {
+            StartCoroutine(enumerator);
+        }
+        public void ChangeSkybox()
+        {
+        }
+        public Scene ForceLoadSubScene(string SceneName)
+        {
+
+            LoadSceneParameters loadSceneParameters = new LoadSceneParameters(LoadSceneMode.Additive);
+            return SceneManager.LoadScene(SceneName, loadSceneParameters);
+        }
+        public void UnloadSubScene(string SceneName)
+        {
+            SceneManager.UnloadSceneAsync(SceneName);
+        }
+        public void UnloadSubsScene(Scene s)
+        {
+            SceneManager.UnloadSceneAsync(s);
+        }
+    }
+
+}
