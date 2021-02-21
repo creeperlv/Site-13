393045ad9b4404dc1325c304e7146321156ac373
Creeper Lv <creeperlv38@outlook.com>
2021-02-21 17:23:04 +0800

    0.1.90.0 -Small adjustments on SBL1-Prt-2 and SBL1-Prt-3. -Fixing the Max Distance of Navigation points. -Added language varied Documents. -Updated Site-13ToolsLib.dll

--- Assets/SCPAssets/Scripts/InfoObject.cs
+++ Assets/SCPAssets/Scripts/InfoObject.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class InfoObject : MonoBehaviour
+namespace Site13Kernel.FPSSystem
 {
-    // Start is called before the first frame update
-    void Start()
+    public class InfoObject : MonoBehaviour
     {
-        
+        public string DocumentID;
+        public string Fallback="[Debug Mode Replacement]";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
