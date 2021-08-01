df6d42cb9fa4aff84adaa8fd0e98b1a5d0aa70e3
Creeper Lv <creeperlv38@outlook.com>
2021-08-01 23:53:29 +0800

    0.1.90.0 -Adjusted the visual of MainMenuV3, it's now more Reach-Style. -Continue working on Campaign System.

--- Assets/SCPAssets/Scripts/GameLogic/CampaignController.cs
+++ Assets/SCPAssets/Scripts/GameLogic/CampaignController.cs
@@ -2,14 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 namespace Site13Kernel.GameLogic
 {
     public class CampaignController : MonoBehaviour
     {
+        public List<string> SubScenes=new List<string>();
+        public List<Scene> _SubScenes=new List<Scene>();
+        public List<AsyncOperation> __SubScenes=new List<AsyncOperation>();
         public List<BehaviorController> behaviorControllers= new List<BehaviorController>();
         void Start()
         {
-
+            foreach (var item in SubScenes)
+            {
+                _SubScenes.Add(SceneManager.GetSceneByName(item));
+            }
+            foreach (var item in _SubScenes)
+            {
+                var AO=SceneManager.LoadSceneAsync(item.name, LoadSceneMode.Additive);
+                AO.allowSceneActivation = false;
+                __SubScenes.Add(AO);
+            }
         }
 
         void Update()
@@ -17,19 +31,24 @@ namespace Site13Kernel.GameLogic
 
         }
     }
-    
+
 }
 namespace Site13Kernel.GameLogic.CampaignActions
 {
     [Serializable]
     public class CampaignAction { }
     [Serializable]
-    public class LoadScene
+    public class LoadSceneByName : CampaignAction
     {
         public string Name;
     }
     [Serializable]
-    public class WaitUntilLastSceneDone
+    public class WaitUntilLastSceneDone : CampaignAction
+    {
+
+    }
+    [Serializable]
+    public class SetCheckpoint : CampaignAction
     {
 
     }
