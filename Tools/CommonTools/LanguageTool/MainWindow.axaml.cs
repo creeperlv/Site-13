823e31238d66132ac26b6e61150d412ace5dded3
Creeper Lv <creeperlv38@outlook.com>
2021-08-10 00:17:17 +0800

    0.1.90.0 -Working on the tools.

--- Tools/CommonTools/LanguageTool/MainWindow.axaml.cs
+++ Tools/CommonTools/LanguageTool/MainWindow.axaml.cs
@@ -1,6 +1,9 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using CommonTools;
+using System.Diagnostics;
 
 namespace LanguageTool
 {
@@ -13,12 +16,35 @@ namespace LanguageTool
             this.AttachDevTools();
 #endif
         }
-
+        void Init()
+        {
+            AboutButton.Click += (_, _) => {
+                Dialog dialog=new Dialog();
+                dialog.DialogTitle = "About";
+                dialog.DialogContent = new TextBlock {Text="Language Tool\nSite-13 Toolset" };
+                dialog.ShowDialog(this);
+            };
+            Load();
+        }
+        void Load()
+        {
+            CentralEditor.Children.Add(new Field());
+            CentralEditor.Children.Add(new Field());
+            CentralEditor.Children.Add(new Field());
+        }
+        StackPanel CentralEditor;
+        Button AboutButton;
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
             this.ExtendClientAreaChromeHints = Avalonia.Platform.ExtendClientAreaChromeHints.PreferSystemChrome;
             this.ExtendClientAreaToDecorationsHint = true;
+            this.TransparencyLevelHint = WindowTransparencyLevel.Blur;
+            Trace.WriteLine(this.ActualTransparencyLevel);
+            this.Background = new SolidColorBrush(Colors.Transparent);
+            CentralEditor=this.FindControl<StackPanel>("CentralEditor");
+            AboutButton = this.FindControl<Button>("AboutButton");
+            Init();
             //this.SystemDecorations = SystemDecorations.BorderOnly;
         }
     }
